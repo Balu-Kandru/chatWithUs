ba0fcc570a2df5b4321dee9b5711a8852009ecd0
Balu_Kandru <swamy.kandru@gmail.com>
2024-07-22 01:28:48 +0530

    solved issue with signup and minor bugs

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -6,13 +6,25 @@ import '../styles/common.css';
 import { apiClient } from "../config";
 import { ApiRoutes } from "../enums/ApiRoutes";
 import { setLocalStorage } from "../assets/utilities";
+import isEmail from 'validator/lib/isEmail';
 
 
 const SignUp: React.FC = () => {
-	const [registerState, setRegisterState] = useState({});
+	const [registerState, setRegisterState] = useState<SignUpI>({
+		username: "",
+		email: "",
+		password: ""
+	});
 	const navigate = useNavigate();
 	const navigateToSignIn = () => {
 		navigate("/");
+	}
+
+
+	interface SignUpI {
+		username: string,
+		email: string,
+		password: string
 	}
 
 	interface User {
@@ -25,7 +37,7 @@ const SignUp: React.FC = () => {
 		createdAt: string;
 		updatedAt: string;
 	}
-	
+
 	interface AuthResponse {
 		jwt: string;
 		user: User;
@@ -34,26 +46,35 @@ const SignUp: React.FC = () => {
 	const [isLargeScreen, setIsLargeScreen] = useState(window.matchMedia("(min-width: 768px)").matches);
 
 	useEffect(() => {
-	  const mediaQuery = window.matchMedia("(min-width: 768px)");
-	  const handleMediaChange = (e: any) => setIsLargeScreen(e.matches);
-  
-	  mediaQuery.addEventListener('change', handleMediaChange);
-  
-	  return () => mediaQuery.removeEventListener('change', handleMediaChange);
+		const mediaQuery = window.matchMedia("(min-width: 768px)");
+		const handleMediaChange = (e: any) => setIsLargeScreen(e.matches);
+
+		mediaQuery.addEventListener('change', handleMediaChange);
+
+		return () => mediaQuery.removeEventListener('change', handleMediaChange);
 	}, []);
 
-	const handleLogin = () => {
-			apiClient.post<ApiRoutes, AxiosResponse<AuthResponse>>(ApiRoutes.LOGIN, registerState)
+	const handleSignUp = () => {
+		if(!isEmail(registerState.email)){
+			alert("email must be a valid email")
+			return;
+		}else if (registerState.password.length <= 5) {
+			alert("password should be atleast 6 characters")
+			return;
+		}else if(registerState.username.length <= 5){
+			alert("username is a required field");
+			return;
+		}
+		apiClient.post<ApiRoutes, AxiosResponse<AuthResponse>>(ApiRoutes.SIGNUP, registerState)
 			.then((loginData: AxiosResponse<AuthResponse>) => {
 				setLocalStorage(loginData.data.jwt, loginData.data.user.username);
 				navigate("/chat")
 			}).catch((err: any) => {
 				alert(`Unauthorized user ${err}`);
-
 			})
 	}
 
-	if(isLargeScreen){
+	if (isLargeScreen) {
 		return (
 			<>
 				<div className="d-flex">
@@ -70,7 +91,7 @@ const SignUp: React.FC = () => {
 								Sign up
 							</div>
 							<div className="width80 mt5">
-								<input id="name1" type="text" placeholder="Name" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, name: e.target.value }) }} />
+								<input id="name1" type="text" placeholder="Name" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, username: e.target.value }) }} />
 							</div>
 							<div className="width80 mt5">
 								<input id="email1" type="email" placeholder="Email" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, email: e.target.value }) }} />
@@ -79,15 +100,15 @@ const SignUp: React.FC = () => {
 								<input id="password1" type="password" className="signUpinput" placeholder="Password" onChange={(e) => { setRegisterState({ ...registerState, password: e.target.value }) }} />
 							</div>
 							<div className="m5">
-								<button type="submit" className="fs24 button" onClick={handleLogin}>Register</button>
+								<button type="submit" className="fs24 button" onClick={handleSignUp}>Register</button>
 							</div>
 						</div>
 					</div>
 				</div>
 			</>
 		)
-	}else{
-		return(
+	} else {
+		return (
 			<>
 				<div className="loginDiv">
 					<div className="HeadingDiv">Chat With Us</div>
@@ -96,7 +117,7 @@ const SignUp: React.FC = () => {
 							Sign up
 						</div>
 						<div className="width80 mt5">
-							<input id="name1" type="text" placeholder="Name" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, name: e.target.value }) }} />
+							<input id="name1" type="text" placeholder="Name" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, username: e.target.value }) }} />
 						</div>
 						<div className="width80 mt5">
 							<input id="email1" type="email" placeholder="Email" className="signUpinput" onChange={(e) => { setRegisterState({ ...registerState, email: e.target.value }) }} />
@@ -105,7 +126,7 @@ const SignUp: React.FC = () => {
 							<input id="password1" type="password" className="signUpinput" placeholder="Password" onChange={(e) => { setRegisterState({ ...registerState, password: e.target.value }) }} />
 						</div>
 						<div className="m5">
-							<button type="submit" className="fs24 button" onClick={handleLogin}>Register</button>
+							<button type="submit" className="fs24 button" onClick={handleSignUp}>Register</button>
 						</div>
 					</div>
 					<div className="signInButtonDiv">
